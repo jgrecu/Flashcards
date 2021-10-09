129099b9cb709777c12ee57d470462eda358ad01
Jeremy Grecu <jeremy.grecu@sap.com>
2021-10-09 14:05:48 +0100

    JBA - Flashcards - Stage 1/7: Stage one, card one

--- task/src/flashcards/Main.java
+++ task/src/flashcards/Main.java
@@ -2,6 +2,9 @@ package flashcards;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("\nCard:");
+        System.out.println("to go");
+        System.out.println("Definition:");
+        System.out.println("ir en Español");
     }
 }
