2ce5b194f3627bee4df3f6e2cc6a54e36b4e6975
Jeremy Grecu <jeremy.grecu@sap.com>
2021-10-09 14:10:48 +0100

    JBA - Flashcards - Stage 2/7: What’s on the card?

--- task/src/flashcards/Main.java
+++ task/src/flashcards/Main.java
@@ -1,10 +1,17 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nCard:");
-        System.out.println("to go");
-        System.out.println("Definition:");
-        System.out.println("ir en Español");
+        final Scanner scanner = new Scanner(System.in);
+//        System.out.println("\nCard:");
+//        System.out.println("to go");
+//        System.out.println("Definition:");
+//        System.out.println("ir en Español");
+        String term = scanner.nextLine();
+        String definition = scanner.nextLine();
+        String answer = scanner.nextLine();
+        System.out.println("Your answer is " + (definition.equals(answer) ? "right!" : "wrong..."));
     }
 }
