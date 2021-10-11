adafeb2e1e3c554669ea9207486ef4c76dfb45cc
Jeremy Grecu <jeremy.grecu@sap.com>
2021-10-11 15:03:17 +0100

    JBA - Flashcards - Stage 6/7: Statistics - add wrongAnswers fiend into Flashcard class

--- task/src/flashcards/Flashcard.java
+++ task/src/flashcards/Flashcard.java
@@ -3,10 +3,12 @@ package flashcards;
 public class Flashcard {
     private String term;
     private String definition;
+    private int wrongAnswers;
 
     public Flashcard(String term, String definition) {
         this.term = term;
         this.definition = definition;
+        this.wrongAnswers = 0;
     }
 
     public String getTerm() {
@@ -23,5 +25,13 @@ public class Flashcard {
 
     public void setDefinition(String definition) {
         this.definition = definition;
+    }
+
+    public int getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public void setWrongAnswers(int wrongAnswers) {
+        this.wrongAnswers = wrongAnswers;
     }
 }
